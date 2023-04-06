c3ea39d53686b916ff292a7738a7ee7a6f006aa5
Raoul <nasj1621@gmail.com>
2023-04-07 03:15:32 +0900

    build sign-up page: mess.ver

--- invenManage/accounts/forms.py
+++ invenManage/accounts/forms.py
@@ -1,8 +1,22 @@
+#https://ordinarycoders.com/blog/article/django-user-register-login-logout
+
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
-class SignupForm(forms.Form):
-    class Meta:
-        fields = ['username', 'email']
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
+# Create your forms here.
+
+class NewUserForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = ("username", "email", "password1", "password2")
+
+	def save(self, commit=True):
+		user = super(NewUserForm, self).save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
--- invenManage/accounts/views.py
+++ invenManage/accounts/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from . models import Account
+from .forms import NewUserForm
+from django.contrib.auth import login
 
-def sign_in_view(request):
-    if request.method == 'GET':
-        account_obj = Account.objects.get(id=2)
-        account_name = account_obj.name
-        account_content = account_obj.content
+def sign_up(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
         
-        return render(request, 'accounts/login.html')
+            return render(request, 'accounts/signup.html')
+        
+    elif request.method == 'GET':
+        return render(request, 'accounts/signup.html')
+
+# def sign_in_view(request):
+#     if request.method == 'GET':
+#         account_obj = Account.objects.get(id=2)
+#         account_name = account_obj.name
+#         account_content = account_obj.content
+        
+#         return render(request, 'accounts/login.html')
 
 # Create your views here.
